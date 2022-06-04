8b166cd8402f1fa83e205d7e2fe19e1bb3702376
LAPTOP-0HQOAFD7\Pranav <pranavkamat11@gmail.com>
2022-06-04 11:23:45 +0530

    validating user inputs using hibernate

--- blog-app-apis/src/main/java/com/pranav/blog/controllers/UserController.java
+++ blog-app-apis/src/main/java/com/pranav/blog/controllers/UserController.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,14 +31,14 @@ public class UserController {
 
 	// POST create user
 	@PostMapping("/")
-	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+	public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto) {
 		UserDto createUserDto = userService.createUser(userDto);
 		return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
 	}
 
 	// PUT update user
 	@PutMapping("/{userId}")
-	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable Integer userId) {
+	public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto, @PathVariable Integer userId) {
 
 		UserDto updateUser = userService.updateUser(userDto, userId);
 		return ResponseEntity.ok(updateUser);
--- blog-app-apis/src/main/java/com/pranav/blog/payloads/UserDto.java
+++ blog-app-apis/src/main/java/com/pranav/blog/payloads/UserDto.java
@@ -1,5 +1,9 @@
 package com.pranav.blog.payloads;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,9 +14,19 @@ import lombok.Setter;
 public class UserDto {
 	
 	private int id;
+	
+	@NotEmpty
+	@Size(min = 4 , message = "username must be minimum of 4 characters")
 	private String name;
+	
+	@Email(message = "email address is not valid")
 	private String email;
+	
+	@NotEmpty
+	@Size(min = 3 , max = 10 , message = "password size must be between 3 to 10 characters")
 	private String password;
+	
+	@NotEmpty
 	private String about;
 
 }
